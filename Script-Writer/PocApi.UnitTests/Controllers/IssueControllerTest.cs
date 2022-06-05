8eeb22606bb1b74429b0fa8fef172c660063a411
DCgreed <dc-man@hotmail.com>
2022-06-05 22:18:44 +0200

    Updated the test project.

---
+++ Script-Writer/PocApi.UnitTests/Controllers/IssueControllerTest.cs
@@ -0,0 +1,21 @@
+﻿
+namespace PocApi.UnitTests.Controllers
+{
+    public class IssueControllerTest
+    {
+
+        [Fact]
+        public void Has_GetFunction()
+        {
+            // Arrange
+            //var mockRepo = new Mock<IssueService>();
+            //var issueController = new IssueController();
+
+            // Act
+            var result = true;
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
